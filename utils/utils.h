eaae1f81d5fedfa023bbbb6b16fbb7a5c419bb75
Andre <andre.fabian.melo@gmail.com>
2023-02-15 15:37:18 -0300

    Trim leading spaces with safe/non-safe versions

--- utils/utils.h
+++ utils/utils.h
@@ -71,3 +71,41 @@ void uppercase_string(char *string) {
     string++;
   }
 }
+
+void trim_leading_spaces(char *string) {
+  int i = 0;
+  while(string[i] == '\n' || string[i] == '\t' || string[i] == ' ') {
+    i++;
+  }
+
+  if(i != 0) {
+    int j = 0;
+    while(string[j + i] != '\0') {
+      string[j] = string[j + i];
+      j++;
+    }
+    string[j] = '\0';
+  }
+}
+
+char *trim_leading_spaces_safe(char *string) {
+  int i = 0, j = 0;
+  
+  while(string[i] == '\n' || string[i] == '\t' || string[i] == ' ') {
+    i++;
+  }
+
+  int len = abs(strlen(string) - i);
+
+  char *new = malloc(sizeof(char)*len);
+
+  while(string[i] != '\0') {
+    new[j] = string[i];
+    j++;
+    i++;
+  }
+
+  new[len] = '\0';
+
+  return new;
+}
